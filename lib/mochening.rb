3f4efa82335fa172b092bfef998cb66f31e5c0a6
BennyHallett <benny.hallett@gmail.com>
2014-03-26 08:05:16 +1100

    Moved to using rspec mocks

--- lib/mochening.rb
+++ lib/mochening.rb
@@ -5,6 +5,11 @@ module Mochening
     def self.from(db)
       raise "Cannot expect anything from a nil database" if db.nil?
       raise "Expected a block" unless block_given?
+      @db = db
+    end
+
+    def [](table)
+      @db.expects(:[]).with(table)
     end
   end
 end
--- spec/mochening_spec.rb
+++ spec/mochening_spec.rb
@@ -1,16 +1,22 @@
 require 'mochening'
 
-RSpec.configure do |config|
-  config.mock_framework = :mocha
-end
-
 describe Mochening do
   it "raises error when called with nil database" do
     expect { Mochening::Expect.from(nil) }.to raise_error(RuntimeError)
   end
 
   it "raises error when called without a block" do
-    db = mock('object')
+    db = double('database')
     expect { Mochening::Expect.from(db) }.to raise_error(RuntimeError)
+  end
+
+  it "sets an expectation on the db that a dataset is created" do
+    db = double('database')
+    expect(db).to receive(:expects) { :[] }
+    expect(db).to receive(:with) { :table }
+
+    Mochening::Expect.from(db) do |dbase|
+      dbase[:table]
+    end
   end
 end
