d64361b01716845389b5812b259d2d60f5d2e7e1
BennyHallett <benny.hallett@gmail.com>
2014-03-25 18:14:11 +1100

    Requiring a block to use Db.expect_from

--- lib/mochening.rb
+++ lib/mochening.rb
@@ -4,6 +4,7 @@ module Mochening
   class Expect
     def self.from(db)
       raise "Cannot expect anything from a nil database" if db.nil?
+      raise "Expected a block" unless block_given?
     end
   end
 end
--- spec/mochening_spec.rb
+++ spec/mochening_spec.rb
@@ -1,8 +1,16 @@
-require 'mocha/api'
 require 'mochening'
+
+RSpec.configure do |config|
+  config.mock_framework = :mocha
+end
 
 describe Mochening do
   it "raises error when called with nil database" do
     expect { Mochening::Expect.from(nil) }.to raise_error(RuntimeError)
+  end
+
+  it "raises error when called without a block" do
+    db = mock('object')
+    expect { Mochening::Expect.from(db) }.to raise_error(RuntimeError)
   end
 end
